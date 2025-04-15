2893d57294dcd54394bcec952b14373a62c0f4b1
Gabe <184721807+gabyel-dev@users.noreply.github.com>
2025-04-15 23:07:23 +0800

    fix some shit

--- sample.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name: str):
-        self.name = name
-        
-def getname(one_person: Person):
-        return one_person.name
-
-p = Person('hey')
-print(getname(p))
